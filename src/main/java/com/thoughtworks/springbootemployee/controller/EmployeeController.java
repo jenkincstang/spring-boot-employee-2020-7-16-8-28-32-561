14e6bd2b04eb1207a5c606b4cbf2df41c35d48bc
jenkincstang <jenkincs@163.com>
2020-07-28 20:16:43 +0800

    [springboot-employee][Lebron] feature：pass api GET /employees?gender=male

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -19,21 +19,33 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{employeeId}")
-    public Employee findEmployeeById(@PathVariable int employeeId){
+    public Employee findEmployeeById(@PathVariable int employeeId) {
         return employeeService.findEmployeeById(employeeId);
     }
 
 
     @GetMapping("/employees")
-    public List<Employee> findEmployeesByPage(@RequestParam("page") int page,@RequestParam("pageSize") int pageSize) {
+    public List<Employee> findEmployeesByPage(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize) {
         List<Employee> employees = employeeService.findAllEmployees();
         List<Employee> pagedEmployees = new ArrayList<>();
 
         int startRow = (page - 1) * pageSize;
         int endRow = startRow + pageSize - 1;
-        for (int index = startRow; index < employees.size() && index < endRow; index ++){
+        for (int index = startRow; index < employees.size() && index < endRow; index++) {
             pagedEmployees.add(employees.get(index));
         }
         return pagedEmployees;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findEmployeesByGender(@RequestParam("gender") String gender) {
+        List<Employee> employees = employeeService.findAllEmployees();
+        List<Employee> employeesWithGender = new ArrayList<>();
+
+        for (int index = 0; index < employees.size(); index++) {
+            if (employees.get(index).getGender().equals(gender))
+                employeesWithGender.add(employees.get(index));
+        }
+        return employeesWithGender;
     }
 }
