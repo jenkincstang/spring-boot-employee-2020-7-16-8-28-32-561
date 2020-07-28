3e64e468525e93c517db32cabba7a1bda3754d2e
jenkincstang <jenkincs@163.com>
2020-07-28 20:12:02 +0800

    [springboot-employee][Lebron] feature：pass api GET

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,5 +21,18 @@ public class EmployeeController {
     @GetMapping("/employees/{employeeId}")
     public Employee findEmployeeById(@PathVariable int employeeId){
         return employeeService.findEmployeeById(employeeId);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findEmployeesByPage(@RequestParam("page") int page,@RequestParam("pageSize") int pageSize) {
+        List<Employee> employees = employeeService.findAllEmployees();
+        List<Employee> pagedEmployees = new ArrayList<>();
+
+        int startRow = (page - 1) * pageSize;
+        int endRow = startRow + pageSize - 1;
+        for (int index = startRow; index < employees.size() && index < endRow; index ++){
+            pagedEmployees.add(employees.get(index));
+        }
+        return pagedEmployees;
     }
 }
